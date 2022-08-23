5ec3dd5c37d67042bd0cee82b4e21a2cdbef9ea4
Anna <anna.vasi4ko@icloud.com>
2022-08-23 14:25:26 +0300

    Initial Commit

---
+++ GaugeApp/GaugeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeAppApp.swift
+//  GaugeApp
+//
+//  Created by Vasichko Anna on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GaugeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
